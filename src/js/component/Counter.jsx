a8bd3a5a1cbd67d02f8b844f0573edfe59b57f91
leoisnothere <82447759+leoisnothere@users.noreply.github.com>
2024-06-21 12:35:53 +0000

    Clock update

--- src/js/component/Counter.jsx
+++ src/js/component/Counter.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faClock} from '@fortawesome/free-regular-svg-icons'
 
 const Counter = () => {
     const [segundoActual, setsegundoActual] = useState(0);
@@ -25,10 +26,9 @@ const Counter = () => {
   return (
     
     <div>
-        <div className="container">
 			<div className="row bg-dark text-light h1 p-3 text-center">
 				<div className="col" id="logo">
-                <i class="fa-solid fa-clock"></i>
+                <FontAwesomeIcon icon={faClock} />
                 </div>
 				<div className="col" id="pos6">0</div>
 				<div className="col" id="pos5">0</div>
@@ -37,7 +37,6 @@ const Counter = () => {
 				<div className="col" id="pos2">0</div>
 				<div className="col" id="pos1">0</div>
 			</div>
-		</div>
     </div>
     
   )
